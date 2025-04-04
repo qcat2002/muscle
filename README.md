82b7698c87e0085f95ef6435e98a91083b52cbdc
Zepeng Zhao <zxz174@student.bham.ac.uk>
2025-04-04 15:40:52 +0100

    update reamdme

--- README.md
+++ README.md
@@ -51,6 +51,12 @@ I need to subtract the negative torque (average) for each sample index. [1 - (-1
 
 
 ----
+## Important Meeting Records
+
+- April 1 2025
+  - Professor.Yeo suggested me to label all axes with their meanings. It is helpful when doing presentations.
+
+----
 ## Log
 
 
--- test_me.py
+++ test_me.py
@@ -8,9 +8,9 @@ path3 = '/Users/zepeng/Project/muscle/processed_data/363/TS01_2/iso_0neutr_max.m
 path4 = '/Users/zepeng/Project/muscle/processed_data/363/TS01_2/iso_20pflx_t01.mat'
 path5 = '/Users/zepeng/Project/muscle/processed_data/363/TS01_2/iso_30pflx_t02.mat'
 mat = loadmat(path2)
-torque_small = mat['Torque']
+# torque_small = mat['Torque']
 # torque_small = torque_small * 150
-print(torque_small.min(), torque_small.max())
+# print(torque_small.min(), torque_small.max())
 mat2 = loadmat(path3)
 torque_large = mat2['Torque']
 mat3 = loadmat(path4)
@@ -20,10 +20,14 @@ torque_large3 = mat4['Torque']
 
 # plt.title('Range of Torque Values from different mat files')
 # plt.title('Example of Small Torque Values')
+plt.figure(dpi=300)
 plt.title('Set a threshold to classify Active Contraction State')
 # plt.plot(torque_small)
 plt.axhline(y=5, color='red', linestyle='--', linewidth=1)
-plt.plot(torque_large)
-plt.plot(torque_large2)
-plt.plot(torque_large3)
+plt.xlabel('Sample Indices')
+plt.ylabel('Torque Values')
+plt.plot(torque_large, label='Torque1')
+plt.plot(torque_large2, label='Torque2')
+plt.plot(torque_large3, label='Torque3')
+plt.legend()
 plt.show()
